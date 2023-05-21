e490aaa80d1a82748afa2f54ad7fa4cf2220deea
Rickycst <creusot.aymeric7@gmail.com>
2023-05-21 10:45:55 +0200

    Add git ignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/aymeric_dev/hangman/venv"
+set -gx VIRTUAL_ENV "/home/ricky/Documents/projetGit/hangman/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
